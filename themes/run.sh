44a41d6486dc52ef56147af835b3dbeb7bc2ad83
No Name <alexandre@labanqui.se>
2023-09-04 08:12:05 +0000

    Deleted 'themes/run.sh' via CloudCannon

--- themes/run.sh
+++
@@ -1,9 +0,0 @@
-if [ ${ENV} = "prod" ]
-then
-pipx run ecoindex-cli analyze --chromedriver-path ./chromedriver --url ${DEV_URL} --recursive --no-interaction --export-format json --max-workers 1 --output-file responsible-digital.json
-node ./scripts/responsible_digital.js
-elif [ ${ENV} = "local" ]
-then
-pipx run ecoindex-cli analyze --chromedriver-path ./chromedriver --url ${DEV_URL} --recursive --no-interaction --export-format json --output-file responsible-digital.json
-node ./scripts/responsible_digital.js
-fi
